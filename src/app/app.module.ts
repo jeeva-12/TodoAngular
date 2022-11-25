a9bcc97347fd190196d22ae148abd1aa60fb5293
jeeva-12 <112372759+jeeva-12@users.noreply.github.com>
2022-11-25 12:26:53 +0530

    dg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoAddComponent } from './todo-add/todo-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoAddComponent
   ],
   imports: [
     BrowserModule,
